d172881a75092b2087f5ab3abce199c9513505f0
cptfran <frankiszymon@gmail.com>
2025-12-10 23:55:31 +0100

    com

--- cython/setup.py
+++ cython/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
 
-
+# com
 setup(
     ext_modules = cythonize("tropicalpy.pyx"),
     include_dirs=[numpy.get_include()]
